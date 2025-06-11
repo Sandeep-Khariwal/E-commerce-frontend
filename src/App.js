5364f7c47de9b634c9b5e8efa16a19c0ca2b9e2e
shubhamThakur01104 <sonujamal01@gmail.com>
2025-06-11 14:45:27 +0530

    Card is ready

--- src/App.js
+++ src/App.js
@@ -5,17 +5,20 @@ import { far } from "@fortawesome/free-regular-svg-icons";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import HeroSection from "./Components/HeroSection/HeroSection";
 import Navbar from "./Components/Navbar/Navbar";
+import Card from "./Components/Card/Card";
 library.add(fab, fas, far);
 
 function App() {
   return (
     <>
       <BrowserRouter>
-          <Navbar/>
+        <Navbar />
         <Routes>
 
           <Route path="/" element={<HeroSection />} />
+          <Route path="/cart" element={<Card />} />
         </Routes>
+        <Card />
       </BrowserRouter>
     </>
   );
