6ed6ea96b140eb286525c13698b35e1bf6ca7788
john <johnsonsarari951@gmail.com>
2025-06-12 13:58:26 +0530

    login page is ready

--- src/Components/LogInPage/LogPage.jsx
+++ src/Components/LogInPage/LogPage.jsx
@@ -1,13 +1,13 @@
 import React, { useEffect, useState } from "react";
 import "../LogInPage/LogPage.css";
-import img1 from "../LogInPage/Images-of-Login/Pic-logo.png";
+import {Link } from 'react-router-dom'
+import image1 from './Images-of-Login/right-box-pic.png'
 
 const Log_in_page = () => {
   const [form, setForm] = useState({
     email: "",
     password: "",
   });
-
 
   function handleChange(field, e) {
     setForm((prev) => {
@@ -18,45 +18,57 @@ const Log_in_page = () => {
     });
   }
 
-  function logvalue(){
+  function logvalue() {
     console.log(form);
   }
 
   return (
     <>
       <div className="main-page">
-        <div className="box1">
-          <div className="text">
-            <div className="text1">Login</div>
-            <div className="under-line"></div>
-            <div class="input-box">
-              <div class="name">
-                <div class="tag-1">
+        <div className="login-box">
+          <div className="left-sidebox">
+            <div className="aline-all-things">
+            <div className="left-side-texts">
+              <div className="logo-box">𝔅BeliBeli.com</div>
+              <div className="display-text">Welcome-to-login</div>
+            </div>
+          </div>
+          <div className="input-section">
+            <div className="input-text">Email</div>
+             <div class="tag-1">
                   <input
                     type="text"
                     onChange={(e) => handleChange("email", e)}
                     placeholder="Enter your email"
                   />
                 </div>
-              </div>
-              <div class="name">
-                <div class="tag-1">
+                <div className="input-text">Password</div>
+                  <div class="tag-1">
                   <input type="password"
                   onChange={(e) => handleChange("password", e)}
                   placeholder="  Enter your password" />
                 </div>
-              </div>
-              <div className="button">
-                <div className="forgot">
-                  <a href="#"> forgot your password ?</a>
+          </div>
+          <div className="button-section">
+              <div className="forgot">
+               
+                <Link to='#' className="forgot-password">forgot your password?</Link>
+                 </div>
+                <div className="sign-button"> create an account -
+                <Link to='#' className="sign-up">Sign-up</Link>
                 </div>
-                <button onClick={logvalue}>Sign-Up</button>
-              </div>
-            </div>
+                </div>
+                <div className="final-button">
+                    <button onClick={logvalue}>Log-In</button>
+              
+                </div>
           </div>
-          <div className="img">
-            <img width={"100%"} height={"100%"} src={img1} />
+
+
+          <div className="right-side-img-box">
+
           </div>
+      
         </div>
       </div>
     </>
